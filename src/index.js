5aa138c14a41ebac2c492935d80198bebfeaa4c7
gogoend <gogoend@qq.com>
2020-03-21 21:57:27 +0800

    清理

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Logo from './logo.svg';
-
 
 function component() {
     var element = document.createElement('div');
 
-    // lodash，现在由此脚本导入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // 将图像添加到我们已经存在的 div 中。
-    var mylogo = new Image();
-    mylogo.src = Logo;
-
-    element.appendChild(mylogo);
 
     return element;
 }
