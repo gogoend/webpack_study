854df9d97447b09e88281b070c3d2fea0d4451d0
gogoend <gogoend@qq.com>
2020-03-21 21:53:04 +0800

    外部图像导入

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import _ from 'lodash';
 import './style.css';
+import Logo from './logo.svg';
+
 
 function component() {
     var element = document.createElement('div');
 
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    // lodash，现在由此脚本导入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    // 将图像添加到我们已经存在的 div 中。
+    var mylogo = new Image();
+    mylogo.src = Logo;
+
+    element.appendChild(mylogo);
+
     return element;
 }
 
