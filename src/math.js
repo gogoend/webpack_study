ab4bb57b081e019f979aa0a1118d0b1e9d3ee94b
gogoend <gogoend@qq.com>
2020-03-22 23:32:41 +0800

    tree shaking测试，sideEffects没啥用？？

---
+++ src/math.js
@@ -0,0 +1,15 @@
+export function square(x) {
+    let a={
+        'square':'平方'
+    }
+    console.log(a)
+    return x * x;
+}
+
+export function cube(x) {
+    let a={
+        'cube':'立方'
+    }
+    console.log(a)
+    return x * x * x;
+}
