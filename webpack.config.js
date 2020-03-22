fe56427f49095bfa1f058753c58ad04ad216a767
gogoend <gogoend@qq.com>
2020-03-22 14:56:24 +0800

    热替换测试

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '管理输出'
-    })
+      title: '模块热替换'
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
