4b8adf861606ec83a7ef71cc136940a9f38e36a8
gogoend <gogoend@qq.com>
2020-03-23 00:37:31 +0800

    将第三方包进行分离

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,10 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
   }
 };
